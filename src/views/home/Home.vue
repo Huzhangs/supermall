380df97a480dea9da90767b8389bad9ce85e5338
zhangshun.hu <15071266264@163.com>
2021-08-10 16:53:45 +0800

    完成feature和tabControl

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,32 +1,81 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <swiper>
-      <swiper-item v-for="(item,index) in banners" :key="index">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="titles"></tab-control>
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import HomeSwiper from './childComponents/HomeSwiper.vue'
+  import RecommendView from './childComponents/RecommendView.vue'
+  import FeatureView from './childComponents/FeatureView.vue'
+
   import NavBar from 'components/common/navBar/TabBar.vue'
-  import { Swiper,SwiperItem } from 'components/common/swiper';
+  import TabControl from 'components/content/tabControl/TabControl.vue'
+
   import { getHomeMultidata } from 'network/home.js'
   
   export default {
     name: "Home",
     components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
       NavBar,
-      Swiper,
-      SwiperItem
+      TabControl
     }, 
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        titles: ['流行','新款','精选']
       }
     }, 
     created() {
@@ -40,8 +89,25 @@
 </script>
 
 <style scoped>
+
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
-    color: #fff
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
+
+  .tab-control {
+    position: sticky;
+    top:44px;
+  }
+
 </style>
